21341fa299857318cf0b3f5bf900deb74ad35f6f
wongsathorn <artnote@hotmail.com>
2019-02-28 11:32:51 +0700

    animalapp 28/02/62 ผูกตัวแปล

--- app/src/main/java/sdu/cs/wongsathorn/animalapp/GameActivity.java
+++ app/src/main/java/sdu/cs/wongsathorn/animalapp/GameActivity.java
@@ -2,12 +2,29 @@ package sdu.cs.wongsathorn.animalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
+    //explicit
+    Button btn1,btn2,btn3, btn4;
+    ImageView questionImageView;
+    ImageButton volumeImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-    }
-}
+
+        //ผูกตัวแปลบน java กับ id บน xml
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        questionImageView = findViewById(R.id.imvQuestion);
+        volumeImageButton = findViewById(R.id.imbVolume);
+
+    }//end oncreate
+
+}//end class
